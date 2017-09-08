4ece3ce22bfc7572f5e26bb06cdc0e6b23ebe39e
erje3158 <jensen.erik.w@gmail.com>
2017-09-08 08:31:17 -0600

    Fixed bug in el_stress_ellip3d.cpp - frac's assembly.cpp doesn't have ability to calculated mass of sample, so removed line from el_stress to compensate

--- source/el_stress_ellip3d.cpp
+++ source/el_stress_ellip3d.cpp
@@ -129,18 +129,14 @@ void el_stress_ellip3d(const char * outputDir,
         
     // Change back to the directory from where this code is being run so that file inputs can be referenced properly
     chdir(cCurrentPath);
-        
-    cout << "HERE 1" << endl;
 
     //Routines to look at comp_progress
     std:ifstream infile(string(outputDir) + "/el"+ to_string(el+1) + "_ip" + to_string(ip+1) + "/comp_progress");
-        
-    cout << "HERE 2" << endl;
 
     //This routine retrieves the last line of comp_progress
     string lastLine = getLastLine(infile);
     // This loop goes through the last line of comp_progress and parses out the approrpriate parts
-    for(ii = 0; ii < 132; ii++) {
+    for(ii = 0; ii < 131; ii++) {
         if(ii == 0)
             trash = stod(lastLine, &sz);
         else {
@@ -150,10 +146,7 @@ void el_stress_ellip3d(const char * outputDir,
             
         if(ii == 25) sig11 = trash;
         if(ii == 128) disp = trash;
-        if(ii == 131) mass = trash;
     }
-        
-    cout << "HERE 3" << endl;
 
     stress_el(0,ip,el) = sig11;
     stress_el(1,ip,el) = sig11;
